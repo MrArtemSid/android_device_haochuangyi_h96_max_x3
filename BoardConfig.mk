1d3ce2ef731c0fb501edd2a32ce182d695b370c3
Bruno Martins <bgcngm@gmail.com>
2024-02-22 16:33:54 +0000

    h96_max_x3: Remove path to AP FW
    Removing the path to AP FW results in not trigerring a reload when
    enabling soft AP, which is actually desired in this case since the
    firmware file is the same.
    
    Change-Id: I3b833e2675ec87d63623f84becb362d606aafcef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,6 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/wifi/6335/fw_bcm4339a0_ag.bin"
 WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/wifi/6335/fw_bcm4339a0_ag.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
