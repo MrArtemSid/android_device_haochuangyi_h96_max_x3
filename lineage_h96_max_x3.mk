6b147f66d32f76881d5990b4f5231ce93a16ea5d
MrArtemSid <artem.sid.key@gmail.com>
2025-02-02 23:27:14 +0400

    h96_max_x3: Update fingerprint

--- lineage_h96_max_x3.mk
+++ lineage_h96_max_x3.mk
@@ -22,12 +22,10 @@ PRODUCT_DEVICE := h96_max_x3
 PRODUCT_MANUFACTURER := Haochuangyi
 PRODUCT_MODEL := H96 Max X3
 PRODUCT_NAME := lineage_h96_max_x3
-PRODUCT_SYSTEM_NAME := h96_max_x3
 
 PRODUCT_GMS_CLIENTID_BASE := android-droid-tv
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="adt3-user 13 TTT1.230205.001 9565391 release-keys" \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := ADT-3/adt3/adt3:13/TTT1.230205.001/9565391:user/release-keys
+    BuildDesc="adt3-user 13 TTT1.230205.001 9565391 release-keys" \
+    BuildFingerprint=ADT-3/adt3/adt3:13/TTT1.230205.001/9565391:user/release-keys \
+    SystemName=h96_max_x3
