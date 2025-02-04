006c9e72b45e7f4d06fdd6c2c89571ceb04968f9
MrArtemSid <artem.sid.key@gmail.com>
2025-02-04 22:52:19 +0400

    h96_max_x3: Add remote controller configs
    This change is mostly required to get mouse mode working

--- device.mk
+++ device.mk
@@ -19,8 +19,11 @@ PRODUCT_HOST_PACKAGES += \
 ## Init-Files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc \
+    $(LOCAL_PATH)/init-files/remote_controllers.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/remote_controllers.rc \
     $(LOCAL_PATH)/init-files/openvfd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/openvfd.rc \
-    $(LOCAL_PATH)/configs/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
+    $(LOCAL_PATH)/configs/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl \
+    $(LOCAL_PATH)/configs/remote/remote-0xfe01.tab:$(TARGET_COPY_OUT_VENDOR)/etc/remote-0xfe01.tab \
+    $(LOCAL_PATH)/configs/remote/remote-0x7f80.tab:$(TARGET_COPY_OUT_VENDOR)/etc/remote-0x7f80.tab
 
 ## TEE
 TARGET_HAS_TEE := false
